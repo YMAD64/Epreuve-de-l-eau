a3016894a3abb686511c63a0d379e5a00283a2ce
Yann <yannmadoire@hotmail.fr>
2023-02-23 12:04:12 +0100

    brouuillon 1

--- brouillon.js
+++ brouillon.js
@@ -1,1 +1,35 @@
-init 
+//MES FONCTIONS
+function ajoutAuTableau(){
+    for(let i=0 ; i<10 ; i++){
+        chM = 0 + i
+
+        for(let j=0 ; j<10 ; j++){
+            chC = 0 + j
+
+            for(let k=0 ; k<10 ; k++){
+                chD = 0 + k
+
+                for(let l=0 ; l<10 ; l++){
+                    chU = 0 + l
+
+                    let nombre1 = `${chM}${chC}`
+                    let nombre2 = `${chD}${chU}`
+                    if (nombre1 < nombre2){
+                        arr.push(`${nombre1} ${nombre2}`)
+                    }
+                }
+            }
+        }
+    } 
+}
+
+let arr = []
+ajoutAuTableau()
+console.log(arr);
+
+function transformerTableauEnChaineEtAfficher(){
+    let chaine = arr.join(', ')
+    console.log(chaine);
+}
+
+transformerTableauEnChaineEtAfficher()
