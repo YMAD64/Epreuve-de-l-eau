42de9c0b4a3f7622b18871bdd978c05d01d31102
Yann <yannmadoire@hotmail.fr>
2023-02-28 16:41:11 +0100

    eau04 à modifier

--- brouillon.js
+++ brouillon.js
@@ -4,15 +4,49 @@
 // 1 ARGUMENTS ET VARIABLES
 
 args = process.argv.slice(2)
+arg1 = args[0]
+arg2 = args[1]
+
 
 // 2 FONCTIONS
 
 
+function includes (argument1 , argument2){
+
+    let indexstart = -1
+    for(let i=0 ; i < arg1.length ; i++){
+        for(let y=0 ; y < arg2.length ; y++){
+            if(arg1[i]==arg2[y]){
+                
+                indexstart = i
+            }
+            break
+        }
+    } console.log(indexstart);
+    retour = true
+        
+        if (indexstart<0){
+            retour = false
+        }
+        else for(let i=indexstart , j=0 ; i<arg1.length , j<arg2.length ; i++ , j++){
+            if(arg1[i]!==arg2[j]){
+                retour =  false
+            }
+        }
+}
+
 
 // 3 GESTION D'ERREUR
 
-
+if(args.length !=2){
+    console.log("error");
+}
 
 
 
 // 4 RESOLUTION
+
+else if(includes(arg1 ,arg2)){
+    console.log(true);
+}
+else console.log(false);
--- brouillon2.js
+++ brouillon2.js
@@ -1,23 +1,75 @@
 const args = process.argv.slice(2)
+arg1 = args[0]
+arg2 = args[1]
 
 
-function nombrePremier(nombre){
+function includes (argument1 , argument2){
 
-    let nbN = false
-        for(let i=2 ; i < nombre ; i++){
-            if(nombre%i == 0){   
-                nbN = true
+    let indexstart = -1
+    for(let i=0 ; i < arg1.length ; i++){
+        for(let y=0 ; y < arg2.length ; y++){
+            if(arg1[i]==arg2[y]){
+                
+                indexstart = i
             }
-            
-            } 
+            break
+        }
+    } console.log(indexstart);
+    retour = true
         
-        if (nbN == true) return false
-        else if (nbN == false) return true
-}
-
-for(let i=args ; i>=args ; i++){
-    if ((nombrePremier(i)) && (i !== args)){
-        console.log(i)
-        break
+        if (indexstart<0){
+            retour = false
+        }
+        else for(let i=indexstart , j=0 ; i<arg1.length , j<arg2.length ; i++ , j++){
+            if(arg1[i]!==arg2[j]){
+                retour =  false
+            }
+        }
+    
+    if(retour==true){
+        return true
+    }
+    else if(retour==false){
+        return false
     }
 }
+
+if(includes(arg1 ,arg2)){
+    console.log(true);
+}
+else console.log(false);
+
+
+
+
+
+
+// for (let i=0 ; i<arg1.length ; i++){
+//   for (let j=0 ; j<arg2.length ; j++){
+//     if ((arg1[i])==(arg2[j])){
+//         console.log("cool");
+//     }
+//   }
+// }
+
+
+
+
+
+
+
+// console.log(arg1);
+// console.log(arg2);
+
+
+
+
+
+
+
+
+// if(args[0].includes(args[1])){
+// console.log(true);
+// }
+// else console.log(false);
+
