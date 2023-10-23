9a11eb627b018ea51e0027f92a7740a220fae5e6
Yann <yannmadoire@hotmail.fr>
2023-10-23 17:16:52 +0200

    23102013

--- eau14.js
+++ eau14.js
@@ -59,3 +59,44 @@ else if (tabVide(args)==false){
     
     console.log(tirASCII(args).join(' '))
 }
+
+
+///////////////////////////////////////////////////////////
+//23/10/23////////////
+
+// const args = process.argv.slice(2)
+
+// function tirASCII(array){
+
+//     for (let i=0 ; i<array.length ; i++){
+        
+//         for (let j=0 ; j<array.length ; j++){
+            
+//             if((array[j].charCodeAt(0)) > (array[i].charCodeAt(0))){
+                
+//                 vmax = array[j]
+//                 array[j] = array[i]
+//                 array[i] = vmax
+            
+//             }
+            
+//             else if ((array[j].charCodeAt(0)) == (array[i].charCodeAt(0))){
+                
+//                 for(let k=0 ; k<array[j].length ; k++){
+                    
+//                     if(((array[j].charCodeAt(k)) > (array[i].charCodeAt(k))) && ((array[j].charCodeAt(k-1)) == (array[i].charCodeAt(k-1)))){
+                        
+//                         vmax = array[j]
+//                         array[j] = array[i]
+//                         array[i] = vmax
+//                     }
+//                 }
+//             }
+//         }
+//     } return array
+    
+// } 
+    
+
+
+// console.log(tirASCII(args));
